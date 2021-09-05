e283d53dfd5c055955d996fc44ea527ede11a5a6
QueenMehra <90136002+QueenMehra@users.noreply.github.com>
2021-09-05 23:19:33 +0530

    Update logomaker.py

--- YoneRobot/modules/logomaker.py
+++ YoneRobot/modules/logomaker.py
@@ -26,7 +26,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./YoneRobot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype("./AlizaProBot/resources/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -34,13 +34,13 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogoByAliza.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By YoneRobot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By AlizaProBot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @KoraSupport, {e}')
+   await event.reply(f'Error Report @MISTY_SUPORTER, {e}')
 
 
 
@@ -60,13 +60,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./YoneRobot/resources/blackbg.jpg')
+    img = Image.open('./AlizaProBot/resources/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./YoneRobot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./AlizaProBot/resources/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -74,13 +74,13 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogoByAliza.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By YoneRobot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By AlizaProBot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @KoraSupport, {e}')
+   await event.reply(f'Error Report @MISTY_SUPORTER, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
